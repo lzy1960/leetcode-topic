51f4702145b6c641010225d6063942880fe6bc96
lzy1960 <1960553899@qq.com>
2022-06-12 21:12:21 +0800

    121-买卖股票的最佳时机

---
+++ 121-买卖股票的最佳时机.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// 1. 暴力解法(超出时间限制)
+// var maxProfit = function (prices) {
+//   let result = 0
+
+//   for (let i = 0; i < prices.length - 1; i++) {
+//     for (let j = i + 1; j < prices.length; j++) {
+//       let delta = prices[j] - prices[i]
+//       if (delta > result) {
+//         result = delta
+//       }
+//     }
+//   }
+//   return result
+// };
+
+// 2. 只遍历一次，每次都记录最低点
+// 然后计算，如果当天卖出能挣的最多的钱
+var maxProfit = function (prices) {
+  let min = Number.MAX_SAFE_INTEGER
+  let maxMoney = 0
+  for (const curPrice of prices) {
+    if (min > curPrice) {
+      min = curPrice
+    } else if (curPrice - min > maxMoney) {
+      maxMoney = curPrice - min
+    }
+  }
+  return maxMoney
+};
