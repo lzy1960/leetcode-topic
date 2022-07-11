13e183ca29d190ec018efa37832213702515ab04
lzy1960 <1960553899@qq.com>
2022-07-11 13:52:12 +0800

    11-盛水最多的容器

---
+++ leetcode/11.盛水最多的容器.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  const len = height.length
+  let l = 0
+  let r = len - 1
+  let max = 0
+
+  while (l < r) {
+    const left = height[l]
+    const right = height[r]
+    const area = Math.min(left, right) * (r - l)
+    max = Math.max(area, max)
+    left < right ? l++ : r--
+  }
+
+  return max
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
