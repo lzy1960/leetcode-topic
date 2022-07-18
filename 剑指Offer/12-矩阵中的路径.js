0b91b06c7c6c3317b5a53658545c34bd1c52e684
lzy1960 <1960553899@qq.com>
2022-07-18 18:07:53 +0800

    12-矩阵中的路径

---
+++ 剑指Offer/12-矩阵中的路径.js
@@ -0,0 +1,43 @@
+// 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// A B C E
+// S F C S
+// A D E E
+// 输出：true
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+  const width = board[0].length
+  const height = board.length
+  // 每个方格都可以是开始遍历的起点 
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (dfs(i, j, 0)) {
+        return true
+      }
+    }
+  }
+  return false
+
+  function dfs (i, j, k) {
+    // 越界并且单词不一致，直接返回false
+    if (i >= height || i < 0 || j >= width || j < 0 || board[i][j] !== word[k]) {
+      return false
+    }
+    // 长度一样，说明找到这个路径了
+    if (k === word.length - 1) {
+      return true
+    }
+    // 遍历到这个字符，就把他置空
+    board[i][j] = ''
+    // 递归上下左右看看是不是符合
+    let res = dfs(i - 1, j, k + 1) || dfs(i + 1, j, k + 1) || dfs(i, j + 1, k + 1) || dfs(i, j - 1, k + 1)
+    board[i][j] = word[k]
+    return res
+  }
+};
+
+console.log(exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], 'ABCCED'))
